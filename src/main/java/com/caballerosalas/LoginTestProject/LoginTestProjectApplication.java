4acebc7a90b809e09ab93c5a113804c861d1d07a
Alejandro Caballero Salas <alejandro@caballerosalas.com>
2023-08-23 12:46:08 -0400

    Initial Commit

---
+++ src/main/java/com/caballerosalas/LoginTestProject/LoginTestProjectApplication.java
@@ -0,0 +1,13 @@
+package com.caballerosalas.LoginTestProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoginTestProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoginTestProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/caballerosalas/LoginTestProject/LoginTestProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.caballerosalas.LoginTestProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoginTestProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
