0d96e3ecdb532ec7ce06487a2c35d247f14cb15f
Alejandro Caballero Salas <alejandro@caballerosalas.com>
2023-08-25 02:46:27 -0400

    email tests

--- src/test/groovy/com/caballerosalas/gltest/tests/EmailUtilsSpecification.groovy
+++ src/test/groovy/com/caballerosalas/gltest/tests/EmailUtilsSpecification.groovy
@@ -1,4 +1,75 @@
 package com.caballerosalas.gltest.tests
 
-class EmailUtilsSpecification {
+import com.caballerosalas.gltest.gluser.EmailUtils
+
+import spock.lang.Specification
+
+
+class EmailUtilsSpecification extends Specification {
+
+    def "should be false with a not an at sign"(){
+        given:
+        def String email = "caballerosalas#gmail.com"
+
+        when:
+        boolean valid = EmailUtils.validateEmailFormat(email)
+
+        then:
+        !valid
+    }
+
+    def "should be false with a not an at username"(){
+        given:
+        def String email = "@gmail.com"
+
+        when:
+        boolean valid = EmailUtils.validateEmailFormat(email)
+
+        then:
+        !valid
+    }
+
+    def "should be false with a not an at domain"(){
+        given:
+        def String email = "caballerosalas@"
+
+        when:
+        boolean valid = EmailUtils.validateEmailFormat(email)
+
+        then:
+        !valid
+    }
+
+    def "should be false with a two or more domains"(){
+        given:
+        def String email = "caballerosalas@gmail.com@gmail.com"
+
+        when:
+        boolean valid = EmailUtils.validateEmailFormat(email)
+
+        then:
+        !valid
+    }
+
+    def "should be false with an invalid domain"(){
+        given:
+        def String email = "caballerosalas@gmailcom"
+
+        when:
+        boolean valid = EmailUtils.validateEmailFormat(email)
+
+        then:
+        !valid
+    }
+
+    def "should be true with an valid user and domain"(){
+        given:
+        def String email = "caballerosalas@gmail.com"
+
+        when:
+        boolean valid = EmailUtils.validateEmailFormat(email)
+
+        then:
+        valid
+    }
 }
