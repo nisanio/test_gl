f32ca974dea1e1309801ecee0545afc8c3663a44
Alejandro Caballero Salas <alejandro@caballerosalas.com>
2023-08-27 12:58:45 -0400

    add getter and setter for name to GLUser

--- src/main/java/com/caballerosalas/gltest/gluser/GLUser.java
+++ src/main/java/com/caballerosalas/gltest/gluser/GLUser.java
@@ -52,12 +52,22 @@ public class GLUser  implements UserDetails {
         return id;
     }
 
+
     public String getEmail() {
         return email;
     }
 
     public void setEmail(String email) {
         this.email = email;
+    }
+
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
     }
 
     @Override
